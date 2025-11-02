0db546885426310853fe770cf65d596281c3c742
ikgoomophuting-del <ikgoomophuting@gmail.com>
2025-11-02 22:17:39 +0200

    Update manage.py

--- manage.py
+++ manage.py
@@ -1,2 +1,19 @@
 python manage.py makemigrations
 python manage.py migrate
+
+from properties.models import Property
+from django.core.cache import cache
+
+# Confirm cache is empty
+print(cache.get('all_properties'))
+
+# Generate and store the cache
+list(Property.objects.all().values())
+cache.set('all_properties', "test_data", 3600)
+print(cache.get('all_properties'))  # Should show 'test_data'
+
+# Create a Property to trigger post_save
+Property.objects.create(title="Test", description="Signal test", price=1000, location="Cape Town")
+
+# Check cache again
+print(cache.get('all_properties'))  # Should now be None (invalidated)
