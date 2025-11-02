43df4459b4c53d4af4f36a3a3b0d6265290e8b62
ikgoomophuting-del <ikgoomophuting@gmail.com>
2025-11-02 22:09:59 +0200

    Update views.py

--- properties/views.py
+++ properties/views.py
@@ -1,13 +1,16 @@
-from django.shortcuts import render
+from django.http import JsonResponse
 from django.views.decorators.cache import cache_page
 from .models import Property
 
-@cache_page(60 * 15)  # Cache this view for 15 minutes
+@cache_page(60 * 15)  # Cache for 15 minutes (900 seconds)
 def property_list(request):
     """
-    View to display all properties.
-    Cached for 15 minutes using Redis as the backend.
+    Returns a JSON response of all properties.
+    The response is cached in Redis for 15 minutes.
     """
-    properties = Property.objects.all()
-    return render(request, 'properties/property_list.html', {'properties': properties})
-  
+    properties = Property.objects.all().values(
+        'id', 'title', 'description', 'price', 'location', 'created_at'
+    )
+    return JsonResponse({
+        "data": list(properties)
+    })
